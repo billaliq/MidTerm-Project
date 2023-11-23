9cc4c7da6a448044c1e178766e8072bd828c644a
Musab <67883529+Musab45@users.noreply.github.com>
2023-11-23 13:04:37 +0500

    Add project files.

---
+++ MidTerm Project/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double salesRate, totalCost;
+	int price, quant;
+	string choice;
+
+	do
+	{
+
+		cout << "\n\n do you want to calcilate";
+		cin >> choice;
+		cout << "\n" << "\n"
+			if (choice == "no")
+			{
+				break;
+			}
+		do
+		{
+			cout << "Enter the Price of product: ";
+			cin >> price;
+
+			cout << "Enter the quantity: ";
+			cin >> quant;
+
+			cout << "Enter the sales rate:"
+			cin >> salesRate;
+
+
+			totalCost = (price *quant) + salesRate;
+
+
+			if (quantity >= 10)
+			{
+				totalCost = totalCost + 0.2;
+			}
+			cout << "\n\n The total cost is = " << totalCost;
+			price++
+		} while (price <= 0);
+	} while (choice == "yes");
+	
+	return 0;
+	
+
+
+	
+}
